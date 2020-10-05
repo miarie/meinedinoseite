2f932c3b5610b74bd25dbb30c12ab6c1226143c4
miarie <72396383+miarie@users.noreply.github.com>
2020-10-05 19:27:27 +0200

    Update

---
+++ js/dino.js
@@ -0,0 +1,7 @@
+//
+const dinoaudio = document.getElementById("dinorawr");
+//Spiel Sound ab und macht Textfenster auf:
+function Rawrrrr() {
+    dinoaudio.play();
+    alert("Ich bin ein Dino");
+}
